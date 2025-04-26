13b23331df18d4e4419600aab20731beb641c013
14thDream <dndelara@proton.me>
2025-04-26 20:43:35 +0800

    Added search bar functionality

--- screens/home.js
+++ screens/home.js
@@ -6,22 +6,40 @@ import {
   TextInput,
   FlatList,
 } from "react-native";
+import { useState } from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import RecipePost from "../components/RecipePost";
 
 import { spacing, fonts } from "../styles";
 import DATA from "../tests/mock";
 
+const isTitleSearchableFrom = (title, searchText) => {
+  return searchText
+    .toLowerCase()
+    .split(" ")
+    .every((word) => title.toLowerCase().includes(word));
+};
+
 const HomeScreen = () => {
+  const [posts, setPosts] = useState(DATA);
+
+  const handleChangeSearchText = (text) => {
+    setPosts(DATA.filter(({ title }) => isTitleSearchableFrom(title, text)));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>FlavorFind</Text>
-        <TextInput style={styles.headerSearchBar} placeholder="Search Recipe" />
+        <TextInput
+          style={styles.headerSearchBar}
+          placeholder="Search Recipe"
+          onChangeText={handleChangeSearchText}
+        />
         <FontAwesome name="search" size={24} color="black" />
       </View>
       <FlatList
-        data={DATA}
+        data={posts}
         renderItem={({ item }) => (
           <View style={styles.recipePost}>
             <RecipePost
