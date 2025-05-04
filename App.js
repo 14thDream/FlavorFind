01e540734ee161abbb4cf79bea86f5601a6a2a0a
14thDream <dndelara@proton.me>
2025-05-04 16:07:11 +0800

    Fixed formatting

--- App.js
+++ App.js
@@ -23,39 +23,41 @@ const App = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const Main = () => {
-    return <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarLabelStyle: { textTransform: "uppercase" },
-          tabBarIcon: ({ size }) => {
-            let iconName;
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarLabelStyle: { textTransform: "uppercase" },
+        tabBarIcon: ({ size }) => {
+          let iconName;
 
-            switch (route.name) {
-              case "My Recipes":
-                iconName = "restaurant";
-                break;
-              case "Create":
-                iconName = "add";
-                break;
-              case "Profile":
-                iconName = "person";
-                break;
-              default:
-                iconName = "home";
-            }
+          switch (route.name) {
+            case "My Recipes":
+              iconName = "restaurant";
+              break;
+            case "Create":
+              iconName = "add";
+              break;
+            case "Profile":
+              iconName = "person";
+              break;
+            default:
+              iconName = "home";
+          }
 
-            return <Ionicons name={iconName} size={size} color="black" />;
-          },
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="My Recipes" component={HomeScreen} />
-        <Tab.Screen name="Create" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={HomeScreen} />
-      </Tab.Navigator>;
+          return <Ionicons name={iconName} size={size} color="black" />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="My Recipes" component={HomeScreen} />
+      <Tab.Screen name="Create" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={HomeScreen} />
+    </Tab.Navigator>
+  );
 };
 
 export default App;
